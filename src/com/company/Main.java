d090c58c54131c937cfef06bcd7a998c162d285a
Vipin Kumar <vipinthakur3881@gmail.com>
2023-01-31 23:55:48 +0530

    Created a basic banking application

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Welcome banking application");
+        System.out.println("Our Operation");
+        System.out.println("click 1 to exit form system");
+        System.out.println("click 2 for money transfer");
+        System.out.println("click 3 to get money");
+        int operation = sc.nextInt();
+
+        if(operation == 1 ){
+            System.out.println("Exiting from system");
+        } else if (operation == 2){
+            System.out.println("You money is transferring");
+        }else if(operation ==3){
+            System.out.println("You got you money");
+        }else{
+            System.out.println("Invalid operation");
+        }
     }
 }
