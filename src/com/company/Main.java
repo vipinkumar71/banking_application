f0362587d865fa96dbcc47953aa1564f56ab996b
Vipin Kumar <vipinthakur3881@gmail.com>
2023-02-01 00:20:00 +0530

    banking bsic application to understatnd the java operatores

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,13 +14,29 @@ public class Main {
         int operation = sc.nextInt();
 
         if(operation == 1 ){
-            System.out.println("Exiting from system");
+            System.out.println("Exiting from the system");
         } else if (operation == 2){
-            System.out.println("You money is transferring");
+            System.out.println("How much money do you have");
+            int balance = sc.nextInt();
+
+            System.out.println("How much money you want to transfer");
+            int transfer = sc.nextInt();
+            if(balance < transfer){
+                System.out.println("You have insufficient balance");
+            }else{
+                System.out.println("You transferred you money");
+                System.out.println("Your current balance is: " +(balance-transfer));
+                System.out.println("Congratulation");
+            }
         }else if(operation ==3){
-            System.out.println("You got you money");
+            System.out.println("Enter you balance");
+            int balance = sc.nextInt();
+            System.out.println("How much money you want to take");
+            int money = sc.nextInt();
+            System.out.println("You got your money :" +money);
+            System.out.println("Your current balance is : " + (balance-money));
         }else{
-            System.out.println("Invalid operation");
+
         }
     }
 }
