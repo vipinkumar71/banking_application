4cb708db7d372ebcc34b13708f3852830ef60bcd
Vipin Kumar <vipinthakur3881@gmail.com>
2023-02-01 13:52:33 +0530

    switch condition practice

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Welcome banking application");
-        System.out.println("Our Operation");
-        System.out.println("click 1 to exit form system");
-        System.out.println("click 2 for money transfer");
-        System.out.println("click 3 to get money");
-        int operation = sc.nextInt();
-
-        if(operation == 1 ){
-            System.out.println("Exiting from system");
-        } else if (operation == 2){
-            System.out.println("You money is transferring");
-        }else if(operation ==3){
-            System.out.println("You got you money");
-        }else{
-            System.out.println("Invalid operation");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your exam Result");
+        int examResult = sc.nextInt();
+        switch(examResult){
+            case 50:
+                System.out.println("You get D Degree");
+                break;
+            case 60:
+                System.out.println("You got C");
+                break;
+            case 75:
+                System.out.println("You got B");
+                break;
+            case 90:
+                System.out.println("You got A");
+                break;
         }
     }
 }
