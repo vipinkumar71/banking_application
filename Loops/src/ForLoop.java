c6ecf613b1bc01ec57a2a9d1f043cfa24dbf3bd1
Vipin Kumar <vipinthakur3881@gmail.com>
2023-02-07 12:25:34 +0530

    loops directory added

---
+++ Loops/src/ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args){
+        int s=0;
+        for(int i=0; i<10; i++){
+            System.out.println("for loop worked "+i+" times");
+        }
+    }
+}
